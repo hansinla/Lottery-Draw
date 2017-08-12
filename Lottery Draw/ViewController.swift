3e97eae2b97621bf93abfbf3993aa414ce552834
Hans van Riet <hansinla@mac.com>
2017-08-11 23:04:01 -0700

    Initial Commit

---
+++ Lottery Draw/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lottery Draw
+//
+//  Created by Hans van Riet on 08/11/2017.
+//  Copyright © 2017 Hans van Riet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
